caf233a4637046b70f368ae1dd66687e4f044776
255731 <255731@edu.p.lodz.pl>
2026-03-19 15:08:26 +0100

    Dodanie override

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -8,7 +8,7 @@ public class HanoiRhymer extends DefaultCountingOutRhymer {
         return totalRejected;
     }
 
-    public void countIn(int in) {
+    @Override public void countIn(int in) {
         if (!callCheck() && in > peekaboo())
             totalRejected++;
         else
