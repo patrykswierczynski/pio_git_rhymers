f57dfc751d6510e865186fb4c7cc1dddce420a7b
Patryk Świerczyński <patryk.swierczynski.dev@gmail.com>
2026-04-07 11:05:07 +0200

    13. w pliku src/test/java/RhymersDemo wydzielono statyczną metodę testRhymers z metody main

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -10,27 +10,29 @@ class RhymersDemo {
 
     public static void main(String[] args) {
         Rhymersfactory factory = new DefaultRhymersFactory();
-        
+        testRhymers(factory);
+    }
+
+    private static void testRhymers(Rhymersfactory factory) {
         DefaultCountingOutRhymer[] rhymers = { factory.getStandardRhymer(), factory.getFalseRhymer(),
                 factory.getFIFORhymer(), factory.getHanoiRhymer()};
-        
+
         for (int i = 1; i < INT; i++)
             for (int j = 0; j < INT1; j++)
                 rhymers[j].countIn(i);
-        
+
         java.util.Random rn = new java.util.Random();
         for (int i = 1; i < INT; i++)
             rhymers[INT1].countIn(rn.nextInt(20));
-        
+
         for (int i = 0; i < rhymers.length; i++) {
             while (!rhymers[i].callCheck())
                 System.out.print(rhymers[i].countOut() + "  ");
             System.out.println();
         }
-        
+
         System.out.println("total rejected is "
                 + ((HanoiRhymer) rhymers[INT1]).reportRejected());
-        
     }
-    
+
 }
