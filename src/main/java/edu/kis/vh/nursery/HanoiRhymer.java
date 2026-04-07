5f07fdb0e631ba75b12d04577b0bb19a18df683a
Patryk Świerczyński <patryk.swierczynski.dev@gmail.com>
2026-04-07 11:34:10 +0200

    17. Dodano dokumentację klasy HanoiRhymer

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,20 +1,45 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Implementacja wariantu CountingOutRhymer inspirowanego problemem wież Hanoi.
+ *
+ * Klasa pozwala dodawać liczby tylko wtedy, gdy są mniejsze lub równe
+ * ostatnio dodanej liczbie. Jeśli liczba jest większa, zostaje odrzucona.
+ * Liczba odrzuceń jest zapisywana w polu totalRejected.
+ */
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
     private int totalRejected = 0;
 
+    /**
+     * Zwraca liczbę odrzuconych elementów.
+     *
+     * @return liczba elementów, które nie zostały dodane do struktury
+     */
     public int reportRejected() {
         return totalRejected;
     }
 
-    @Override public void countIn(int in) {
+    /**
+     * Dodaje element do struktury tylko wtedy,
+     * gdy jest mniejszy lub równy ostatniemu elementowi.
+     * W przeciwnym razie zwiększa licznik odrzuceń.
+     *
+     * @param in liczba, która ma zostać dodana do struktury
+     */
+    @Override
+    public void countIn(int in) {
         if (!callCheck() && in > peekaboo())
             totalRejected++;
         else
             super.countIn(in);
     }
 
+    /**
+     * Zwraca liczbę odrzuconych elementów.
+     *
+     * @return liczba odrzuconych wartości
+     */
     public int getTotalRejected() {
         return totalRejected;
     }
